77e6dddb2b412597fc46a8125b0c7eb9d34634e1
kashyap9188 <85735044+kashyap9188@users.noreply.github.com>
2022-09-19 22:09:47 +0530

    Create lab1_1.dart

---
+++ Lab1/lab1_1.dart
@@ -0,0 +1,53 @@
+
+/*
+ void main(List<String> arguments)
+{
+   print('Hello world: ${lab1_tutorial1.calculate()}!');
+}
+*/
+
+
+
+/* void main()
+{
+  print('We Staring the Learning of Dart & Flutter');
+}
+Output: on console 'We Staring the Learning of Dart & Flutter' is printed
+*/
+
+
+
+
+/*
+void main()
+{
+  int i;
+  for (i = 0; i < 5; i++)
+    print("hello ${i + 1}");
+}
+
+Output:    hello 1
+          hello 2
+          hello 3
+          hello 4
+          hello 5
+
+*/
+
+
+
+/*
+void main() 
+{
+  stdout.write('not a new line ');
+  print('hello Dart/Flutter');
+  print(calculate());
+}
+
+int calculate() {
+  return 9 * 9;
+}
+
+Output: not a new line hello Dart/Flutter
+       81
+*/
