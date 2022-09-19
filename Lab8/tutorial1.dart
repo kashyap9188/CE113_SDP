5672b384fe6194afc671accf71d32b7d1753fc06
kashyap9188 <85735044+kashyap9188@users.noreply.github.com>
2022-09-19 22:59:20 +0530

    Create tutorial1.dart

---
+++ Lab8/tutorial1.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: FinalTest1(),
+    ));
+
+class FinalTest1 extends StatelessWidget {
+// const FinalTest1({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightGreen[200],
+      appBar: AppBar(
+        title: Text('First App'),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue[600],
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage('assets/sub_assets/dog.jpg')
+            ),
+            Text(
+              'NAME: ',
+              style: TextStyle(
+                color: Colors.grey[800],
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'AYUSH PATEL',
+              style: TextStyle(
+                color: Colors.blue[900],
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
+            ),
+            SizedBox(height: 40),
+            Text(
+              'AGE',
+              style: TextStyle(
+                color: Colors.grey[800],
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '20',
+              style: TextStyle(
+                color: Colors.blue[900],
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
+            ),
+            SizedBox(height: 50),
+            Row(
+              children: [
+                Icon(
+                  Icons.email_rounded,
+                  color: Colors.deepPurple[800],
+                ),
+                SizedBox(width: 12.0),
+                Text(
+                  '20ceuon118.ce@ddu.ac.in',
+                  style: TextStyle(
+                    color: Colors.brown[800],
+                    fontSize: 16.0,
+                    letterSpacing: 1.5,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
