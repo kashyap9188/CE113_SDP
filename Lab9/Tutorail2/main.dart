0809021975d8886b148cf1a2844f74b36929baf4
kashyap9188 <85735044+kashyap9188@users.noreply.github.com>
2022-09-21 12:31:46 +0530

    Update main.dart

--- Lab9/Tutorail2/main.dart
+++ Lab9/Tutorail2/main.dart
@@ -1,32 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:lab9_2/pages/home.dart';
+import 'package:lab9_2/pages/Choose_location.dart';
+import 'package:lab9_2/pages/loading.dart';
 
-import 'choose_location.dart';
-import 'home.dart';
-import 'loading.dart';
 /*
 void main() => runApp(MaterialApp(
-// home: Home(),
-routes: { // routes is Map variable with 'key:value' pairs
-// '/about' ....eg.....routes the screen through 'about' widgets
-// '/contact'....
-'/': (context) => Loading(), // base routes....base widget file...main
-file of project
-// above statement will creates error...because it conflict with 'home:
-Home(),
-// because both statements tell flutter to initialize the app from their
-given location
-'/home': (context) => Home(),
-'/location': (context) => ChooseLocation(),
-}
+  home: Home(),
 ));
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: Text('HOME SCREEN')),
+    );
+  }
+}
 */
 
-
 void main() => runApp(MaterialApp(
-// home: Home(),
-// instead of making home: property to make any page to initialize at beginning...
-// we can use following code ....
-    initialRoute: '/location',
+    initialRoute: '/home',
     routes: {
       '/': (context) => Loading(),
       '/home': (context) => Home(),
